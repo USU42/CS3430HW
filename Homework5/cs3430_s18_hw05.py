4571cc20907a3701a62a01d5ca62d6ba267a1d4d
Kelsye Anderson <kelsyeanderson@Kelsyes-MacBook-Air.local>
2018-02-15 16:21:33 -0700

    worked on first function more

--- Homework5/cs3430_s18_hw05.py
+++ Homework5/cs3430_s18_hw05.py
@@ -2,7 +2,7 @@
 
 ##########################################
 ## module: cs3430_s18_hw05.py
-## Kelsye
+## Kelsye Anderson
 ## A02093326
 ##########################################
 
@@ -10,24 +10,22 @@ import numpy as np
 
 def reduceToREF(m):
     ## your code
-    for x in range(len(m)):
-        m[0][x] *= (1/2)
+    numRows = m
+    numCol = m[0]
+    for i in range(len(numRows)):
+        
+        if m[i][i] is not 0:
+            for x in range(len(numCol)):
+                m[i][x] = m[i][x]/m[i][i]
+        for y in range(i+1, len(numRows)):
+            if m[y][0] is not 0:
+                for z in range(len(numCol)):
+                    m[i+1][z] = (-m[i+1][z] * m[i][z]) + m[i+1][z]
 
-    for x in range(len(m)):
-        m[1][x] += -2 * m[0][x]
-        m[2][x] += -4 * m[0][x]
-
-    for x in range(len(m)):
-        m[2][x] += m[1][x]
-        m[3][x] += m[1][x]
-
-    for x in range(len(m)):
-        m[0][x] += -1 * m[1][x]
     pass
 
 def solveLinSys(m):
    ## your code
-   if m[1][1]
    pass
 
 def invertMat(m):
@@ -62,11 +60,20 @@ reduceToREF(m2)
 
 print(m2, "\n")
 
-
-
-            
-    
-
-
+# for x in range(len(m)):
+#     m[0][x] *= (1 / 2)
+#
+# for x in range(len(m)):
+#     m[1][x] += -2 * m[0][x]
+#     m[2][x] += -4 * m[0][x]
+#
+# for x in range(len(m)):
+#     m[2][x] += m[1][x]
+#     m[3][x] += m[1][x]
+#
+# for x in range(len(m)):
+#     m[0][x] += -1 * m[1][x]
+#
+# for x in m:
     
 
