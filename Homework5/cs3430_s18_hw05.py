b62a93d3be8b9f2d5bfa586acb57e7a73665f4be
Kelsye Anderson <kelsyeanderson@Kelsyes-MacBook-Air.local>
2018-02-14 17:04:34 -0700

    added homework 5

---
+++ Homework5/cs3430_s18_hw05.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+##########################################
+## module: cs3430_s18_hw05.py
+## YOUR NAME
+## YOUR A-NUMBER
+##########################################
+
+import numpy as np
+
+def reduceToREF(m):
+    ## your code
+    pass
+
+def solveLinSys(m):
+   ## your code
+   pass
+
+def invertMat(m):
+    ## your code
+    pass
+
+
+        
+
+
+
+
+
+
+
+
+            
+    
+
+
+    
+
