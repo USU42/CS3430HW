80722e1da6d264c19f791fed012709e3e92bfa49
Kelsye Anderson <kelsyeanderson@Kelsyes-MacBook-Air.local>
2018-02-15 19:26:55 -0700

    finished reduceToREF functiongit add .

--- Homework5/cs3430_s18_hw05.py
+++ Homework5/cs3430_s18_hw05.py
@@ -10,22 +10,45 @@ import numpy as np
 
 def reduceToREF(m):
     ## your code
-    numRows = m
-    numCol = m[0]
-    for i in range(len(numRows)):
-        
-        if m[i][i] is not 0:
-            for x in range(len(numCol)):
-                m[i][x] = m[i][x]/m[i][i]
-        for y in range(i+1, len(numRows)):
-            if m[y][0] is not 0:
-                for z in range(len(numCol)):
-                    m[i+1][z] = (-m[i+1][z] * m[i][z]) + m[i+1][z]
+
+    curCol = 0
+    numRows = len(m)
+    numCol = len(m[0])
+
+    for x in range(numRows):
+        if curCol >= numCol:
+            return
+        curRow = x
+        while m[curRow][curCol] == 0:
+            curRow += 1
+            if curRow == numRows:
+                curRow = x
+                curCol += 1
+                if curCol == numCol:
+                    return
+        temp = m[curRow]
+        m[curRow] = m[x]
+        m[x] = temp
+        pivot = m[x][curCol]
+        m[x] /= float(pivot)
+        for num in range(numRows):
+            if num != x:
+                pivot = m[num][curCol]
+                temp = m[x] * pivot
+                m[num] = m[num] - temp
+        curCol += 1
 
     pass
 
 def solveLinSys(m):
    ## your code
+   reduceToREF(m)
+   numRows = len(m)
+   for x in range(numRows):
+       if m[x] == 0:
+           return False
+
+   return True
    pass
 
 def invertMat(m):
@@ -60,6 +83,15 @@ reduceToREF(m2)
 
 print(m2, "\n")
 
+
+
+
+
+
+
+
+
+
 # for x in range(len(m)):
 #     m[0][x] *= (1 / 2)
 #
@@ -75,5 +107,16 @@ print(m2, "\n")
 #     m[0][x] += -1 * m[1][x]
 #
 # for x in m:
-    
 
+
+# numRows = m
+# numCol = m[0]
+# for i in range(len(numRows)):
+#
+#     if m[i][i] is not 0:
+#         for x in range(len(numCol)):
+#             m[i][x] = m[i][x]/m[i][i]
+#     for y in range(i+1, len(numRows)):
+#         if m[y][0] is not 0:
+#             for z in range(len(numCol)):
+#                 m[i+1][z] = (-m[i+1][z] * m[i][z]) + m[i+1][z]
