a4fcb35f69fc6f6a7720010abeb5a6fe81750fbc
Kelsye Anderson <kelsyeanderson@Kelsyes-MacBook-Air.local>
2018-02-22 09:56:49 -0700

    Created Homework6 folder and added provided code

---
+++ Homework6/cs3430_s18_hw06.py
@@ -0,0 +1,49 @@
+#/usr/bin/python
+
+##########################################
+# module: cs3430_s18_hw06.py
+# YOUR NAME
+# YOUR A-NUMBER
+##########################################
+
+import numpy as np
+
+def minorMat(mat, i, j):
+    ## your code
+    pass
+
+def det(mat):
+    ## your code
+    pass
+
+def cofactor(mat, i, j):
+    ## your code
+    pass
+
+def expandByRowMinors(mat, r):
+    ## your code
+    pass
+
+def expandByColMinors(mat, c):
+    ## your code
+    pass
+
+def cofactorMat(mat):
+    ## your code
+    pass
+
+def adjointMat(mat):
+    ## your code
+    pass
+
+def inverseMat(mat):
+    ## your code
+    pass
+
+def cramer(A, b):
+    ## your code
+    pass
+    
+    
+
+        
