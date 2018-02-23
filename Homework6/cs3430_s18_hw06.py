e7ec576a1efbc0d647ad0359d5bb6203a47af7bb
Kelsye Anderson <kelsyeanderson@Kelsyes-MacBook-Air.local>
2018-02-22 18:20:01 -0700

    finished minorMat function

--- Homework6/cs3430_s18_hw06.py
+++ Homework6/cs3430_s18_hw06.py
@@ -2,19 +2,25 @@
 
 ##########################################
 # module: cs3430_s18_hw06.py
-# YOUR NAME
-# YOUR A-NUMBER
+# Kelsye Anderson
+# A02093326
 ##########################################
 
 import numpy as np
 
 def minorMat(mat, i, j):
     ## your code
-    pass
+    temp = mat
+    temp = np.delete(temp, np.s_[i], 0)#row
+    temp = np.delete(temp, np.s_[j], 1) #column
+    return temp
 
 def det(mat):
     ## your code
-    pass
+    if (len(mat) > 2):
+        det(minorMat(mat, , ))
+    return (mat[0][0] * mat[1][1]) - (mat[0][1] - mat[1][0])
+
 
 def cofactor(mat, i, j):
     ## your code
@@ -45,5 +51,3 @@ def cramer(A, b):
     pass
     
     
-
-        
