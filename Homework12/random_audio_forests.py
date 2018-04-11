72a50f9cda9f53d10d6edbd4f2a170b9b029a792
kelsyeanderson <kelsye.anderson@aggiemail.usu.edu>
2018-04-11 23:00:28 +0000

    worked on train_test_split_eval_rf

--- Homework12/random_audio_forests.py
+++ Homework12/random_audio_forests.py
@@ -17,9 +17,9 @@ import os
 import glob
 import argparse
 
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 from sklearn import metrics
-from sklearn.model_selection import KFold
+from sklearn.cross_validation import KFold
 import matplotlib.colors
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
@@ -27,7 +27,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 
 ## change the base_dir as needed
-base_dir = '/home/Desktop/Homework/CS3430HW/Homework12/audio_data/'
+base_dir = '/home/pi/Desktop/Homework/CS3430HW/Homework12/audio_data/'
 sub_dir = ['beefv','cricketfv','noisefv']
 
 ## Reading the files and creating feature and response object
@@ -46,7 +46,15 @@ def read_audio_data(base_dir):
 
 def train_test_split_eval_dtr(dtr, data, target, test_size=0.4):
     ## your code
-    pass
+    train_data, test_data, train_target, test_targe = \
+                train_test_spilt(data, target, test_size = test_size, random_state = randint(0, 1000))
+    DecisionTreeClassifier()
+    print('train data\'s size %d' % train_data.shape[0])
+    print('train target: %s' % str(train_target))
+    print('test target: %d' % test_data.shape[0])
+    print('test target: %s' % str(test_target))
+    dt = classifier.fit(train_data, train_target)
+    print(sum(dt.predict(test_data) == test_target)/float(len(test_target)))
 
 def train_test_split_eval_rf(rf, data, target, test_size=0.4):
     ## your code
@@ -54,6 +62,7 @@ def train_test_split_eval_rf(rf, data, target, test_size=0.4):
 
 def train_test_split_dtr_range_eval(n, data, target):
     ## your code
+    
     pass
 
 def train_test_split_rf_range_eval(lower_tree_bound, upper_tree_bound, data, target):
